17d0a98ee598ff4732a05f3e6599e5acf9d49a61
chirzarahman <chirzaxrpl2018smkrus@gmail.com>
2020-09-05 17:26:23 +0700

    onbackpreset,login,register

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -29,7 +29,7 @@ public class HomeActivity extends AppCompatActivity {
     Adapter adapter;
 
     Button btn;
-   boolean sd;
+   boolean sds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
